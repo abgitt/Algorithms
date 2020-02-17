12b6597f9cca7d7a2555e3de4350092f34409c70
abgitt <42399185+abgitt@users.noreply.github.com>
2020-02-17 11:21:01 +0530

    InsertionSort
    Added insertiion sort code

---
+++ Sortings/src/AllSortingAlgo/CommonFunctionalities.java
@@ -0,0 +1,18 @@
+package AllSortingAlgo;
+
+public class CommonFunctionalities 
+{
+	public void displayArray(int[] arr)
+	{
+		System.out.println("*******************");
+		System.out.println();
+		for(int i:arr)
+		{
+			System.out.print(i+"  ");
+		}
+		System.out.println();
+		System.out.println();
+
+		System.out.println("*******************");
+	}
+}
---
+++ Sortings/src/AllSortingAlgo/InsertionSort.java
@@ -0,0 +1,47 @@
+package AllSortingAlgo;
+
+public class InsertionSort {
+
+	public static void main(String[] args) 
+	{
+
+		CommonFunctionalities obj = new CommonFunctionalities();
+				int[] arr = new int[] {21,23,12,34,2,7};
+				ascendingSort(arr);
+				obj.displayArray(arr);
+				descendingSort(arr);
+				obj.displayArray(arr);
+
+
+			}
+			public static void ascendingSort(int[] arr)
+			{
+				for(int j=1;j<arr.length;j++)
+				{
+					int key = arr[j];
+					int i=j-1;
+					while(i>=0 && arr[i]>key)
+					{
+						arr[i+1]=arr[i--];
+					}
+					arr[i+1]=key;
+				}
+			}
+			public static void descendingSort(int[] arr)
+			{
+				for(int j=1;j<arr.length;j++)
+				{
+					int key = arr[j];
+					int i=j-1;
+					while(i>=0 && arr[i]<key)
+					{
+						arr[i+1]=arr[i--];
+					}
+					arr[i+1]=key;
+					System.out.println();
+
+				}
+			}
+		
+
+}
